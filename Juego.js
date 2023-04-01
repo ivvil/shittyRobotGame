673092482d80789707a294a009d0220ee1fef108
Iván Villagrasa Pérez <ivvil412@gmail.com>
2023-04-01 17:45:41 +0200

    Added animations for player entity

--- Juego.js
+++ Juego.js
@@ -18,43 +18,71 @@ class entity{
 }
 
 class protagonist extends entity{
-    constructor(x = 0, y = 0, azimuth = 0, hp = 100,inventory = []){
+    constructor(x = 0, y = 0, azimuth = 0, hp = 100, movementSpeed = 10, inventory = []){
 	super(x, y, azimuth, hp);
+	this.movementSpeed = movementSpeed;
 	this.inventory = inventory;
 	this.sprite = new Image();
 	this.sprite.src = "src/sprites/Personajes/move without FX.png"
     }
 }
 
-speed = 5
+playerSpriteStatus = 0
+intervalCtx = 0
 
 function keyDown(event){
-    ctx.clearRect(0, 0, canvas.width, canvas.height) // For now we clean the canvas before any player movement, this doesen't account for any other action that would need to update the canvas
+
     switch(event.keyCode){
     case 87:
-	player.move(0, -speed, 0);
+	player.move(0, -player.movementSpeed, 0);
 	break;
     case 83:
-	player.move(0, speed, 0);
+	player.move(0, player.movementSpeed, 0);
 	break;
     case 65:
-	player.move(-speed, 0, 0);
+	player.move(-player.movementSpeed, 0, 0);
 	break;
     case 68:
-	player.move(speed, 0, 0);
+	player.move(player.movementSpeed, 0, 0);
 	break;
     }
     
 }
-function bigChungus(){
-    ctx.drawImage(player.sprite, 10, 0, 35, 30, player.x, player.y, 50, 50) // Start player on the top left
+function drawCanvas(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.drawImage(player.sprite, 0, playerSpriteStatus * 22, 21, 22, player.x, player.y, 42, 44) // Start player on the top left
+}
+
+function doGlobalAnimationTick(value){
+    if (value){
+	if (intervalCtx == 0) {
+	    intervalCtx = setInterval(globalAnimationTick, 500)
+	} else {
+	    console.log("Loop has already started")
+	}
+    } else {
+	clearInterval(intervalCtx)
+	intervalCtx = 0
+    }
+}
+
+function globalAnimationTick(){
+    // Player's animations
+    if (playerSpriteStatus !== 7) {
+	playerSpriteStatus++
+    } else {
+	playerSpriteStatus = 0
+    }
+    
 }
 
 function onStart(){
     player = new protagonist()
     canvas = document.getElementById("viewport");
     ctx = canvas.getContext("2d");
-    sexo = setInterval(bigChungus, 7)
+    setInterval(drawCanvas, 7)
+    doGlobalAnimationTick(1)
+    // setInterval(globalAnimationTick, 500)
     window.onkeydown=keyDown;
     // window.onkeyup=keyUp;
 }
